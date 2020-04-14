c3056aebb3c3f634f5668adc6237d0287e35230b
John Hutchins <johnwallshutchins@gmail.com>
2020-04-14 12:57:40 -0400

    get it working

--- background_script.js
+++ background_script.js
@@ -3,9 +3,8 @@ const devSuffix = '/_ui/common/apex/debug/ApexCSIPage'
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
         let url = tabs[0].url
-        // window.alert('HOST is: ' + window.location.host)
-        window.alert('URL is: ' + url)
-        return chrome.tabs.create({ 'url': url + devSuffix })
+        let n = url.split('/lightning')[0]
+        return chrome.tabs.create({ 'url': n + devSuffix })
     });
 });
 
