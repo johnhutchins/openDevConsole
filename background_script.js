4ea0ba5ffa9763288cb9e5760ee4b1f18c202886
John Hutchins <johnwallshutchins@gmail.com>
2020-04-14 14:02:00 -0400

    use hostname

--- background_script.js
+++ background_script.js
@@ -3,12 +3,7 @@ const devSuffix = '/_ui/common/apex/debug/ApexCSIPage'
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
         let url = tabs[0].url
-        let n
-        if (url.includes('/lightning')) {
-            n = url.split('/lightning')[0]
-        } else if (url.includes('/home')) {
-            n = url.split('/home')[0]
-        }
+        let n = url.split(window.location.host)[0]
         return chrome.tabs.create({ 'url': n + devSuffix })
     });
 });
