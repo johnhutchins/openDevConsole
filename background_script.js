18be1ade6678508620ff254062e3dcd8b7b0ca9d
John Hutchins <johnwallshutchins@gmail.com>
2020-04-14 12:27:32 -0400

    remove comments

--- background_script.js
+++ background_script.js
@@ -2,9 +2,7 @@
 const devSuffix = '/_ui/common/apex/debug/ApexCSIPage to current url'
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-        let url = tabs[0].url;
-        // use `url` here inside the callback because it's asynchronous!
-        console.log('HOST === ', window.location.host)
+        let url = tabs[0].url
         return chrome.tabs.create({ 'url': url + devSuffix })
     });
 });
