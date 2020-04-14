cc58e6aefbbec4dba64660df1270e859890449ee
John Hutchins <johnwallshutchins@gmail.com>
2020-04-14 14:16:29 -0400

    remove issues

--- background_script.js
+++ background_script.js
@@ -3,8 +3,7 @@ const devSuffix = '/_ui/common/apex/debug/ApexCSIPage'
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
         let url = tabs[0].url
-        const matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
-        window.alert(matches[0])
+        const matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i)
         return chrome.tabs.create({ 'url': matches[0] + devSuffix })
     });
 });
