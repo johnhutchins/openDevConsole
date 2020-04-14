22d5661b89ad5f668351a36b5f1b884fdffe9eb2
John Hutchins <johnwallshutchins@gmail.com>
2020-04-14 13:41:19 -0400

    select for lightning and classic

--- background_script.js
+++ background_script.js
@@ -3,7 +3,12 @@ const devSuffix = '/_ui/common/apex/debug/ApexCSIPage'
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
         let url = tabs[0].url
-        let n = url.split('/lightning')[0]
+        let n
+        if (url.includes('/lightning')) {
+            n = url.split('/lightning')[0]
+        } else if (url.includes('/home')) {
+            n = url.split('/home')[0]
+        }
         return chrome.tabs.create({ 'url': n + devSuffix })
     });
 });
