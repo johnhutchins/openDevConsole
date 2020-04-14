f8616562889b0259fbc6ab15a06f4f8c47fe8ce2
John Hutchins <johnwallshutchins@gmail.com>
2020-04-14 12:49:53 -0400

    debugging

--- background_script.js
+++ background_script.js
@@ -1,9 +1,10 @@
 
-const devSuffix = '/_ui/common/apex/debug/ApexCSIPage to current url'
+const devSuffix = '/_ui/common/apex/debug/ApexCSIPage'
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
         let url = tabs[0].url
-        console.log(window.location.host)
+        // window.alert('HOST is: ' + window.location.host)
+        window.alert('URL is: ' + url)
         return chrome.tabs.create({ 'url': url + devSuffix })
     });
 });
