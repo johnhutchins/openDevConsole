f3b1f9971ff21a0d409de65e8bddda4b32a91e05
John Hutchins <johnwallshutchins@gmail.com>
2020-04-16 20:51:00 -0400

    dont allow url to be changed programmatically

--- background_script.js
+++ background_script.js
@@ -2,7 +2,7 @@
 const devSuffix = '/_ui/common/apex/debug/ApexCSIPage'
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-        let url = tabs[0].url
+        const url = tabs[0].url
         const matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i)
         return chrome.tabs.create({ 'url': matches[0] + devSuffix })
     })
